9f5c325efac8a48f4efa579d8fb71576edd014dc
Sepulveda <sepulvi@gcsnc.com>
2021-11-12 15:59:01 -0500

    front changes

--- controllers/blogPost/home.js
+++ controllers/blogPost/home.js
@@ -1,7 +1,7 @@
 const BlogPost = require('../../models/Post')
 module.exports = async (req, res) => {
     const title = 'Welcome'
-    const blogposts = await BlogPost.find({}).lean()
+    const blogposts = await BlogPost.find({user: req.session.userId}).lean()
     res.render('index', {
         title: title,
         blogposts
